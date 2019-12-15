edf825a3447a29a62542645a53a8f51b1520c7e0
Weerachai <s6102041520165@email.kmutnb.ac.th>
2019-12-15 20:46:12 +0700

    init file

---
+++ views/CreateCourse.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+
+
+class CreateCourse extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <h1>Create Course</h1>
+      </div>
+    )
+  }
+}
+
+export default CreateCourse
