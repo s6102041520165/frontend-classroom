028045b6d4c7cfddb851709915592e0b2aef3506
Weerachai <s6102041520165@email.kmutnb.ac.th>
2019-12-15 22:01:36 +0700

    init app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import CreateCourse from "./views/CreateCourse";
 import UpdateCourse from "./views/UpdateCourse";
+import { width } from 'dom-helpers';
 
 class App extends Component {
   render() {
     return (
-      <div className="App container">
+      <div style={{margin:'auto',maxWidth:"800px",textAlign:'center'}}>
         <Route path="/create-course" component={CreateCourse} />
         <Route path="/update-course" component={UpdateCourse} />
       </div>
--- src/views/CreateCourse.js
+++ src/views/CreateCourse.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import Button from '@material-ui/core/Button';
+import TextField from "@material-ui/core/TextField";
 
 
 class CreateCourse extends Component {
   render() {
     return (
-      <div className="App container">
+      <div>
         <h1>Create Course</h1>
+        <form autoComplete="off">
+          <p>
+            <TextField id="name" style={{width:"80%"}} label="Course name" />
+          </p>
+          <p>
+            <TextField id="room" style={{width:"80%"}} label="Room" />
+          </p>
+          <p>
+            <TextField id="title" style={{width:"80%"}} label="Title" />
+          </p>
+          <p>
+            <Button variant="contained" color="primary">
+              Create course.
+            </Button>
+          </p>
+        </form>
+
       </div>
     )
   }
 }
+
 
 export default CreateCourse
