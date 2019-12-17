712a96c127d3a2d9c7e72bc64de4490e8a3d7c9e
Weerachai <s6102041520165@email.kmutnb.ac.th>
2019-12-17 08:35:27 +0700

    test

--- src/views/CreateCourse.js
+++ src/views/CreateCourse.js
@@ -4,16 +4,41 @@ import TextField from "@material-ui/core/TextField";
 
 
 class CreateCourse extends Component {
+  state = {
+    name: '',
+    section: '',
+    title: '',
+  }
+
+  handleChange = event => {
+    this.setState({
+      name: event.target.value, 
+      section: event.target.value,
+      title: event.target.value,
+    });
+
+    //console.log(name)
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const classroom = {
+      name: this.state.name,
+      section: this.state.section,
+      title: this.state.title,
+    }
+  }
   render() {
     return (
       <div>
         <h1>Update Course</h1>
         <form autoComplete="off">
           <p>
-            <TextField id="name" style={{ width: "80%" }} label="Course name" />
+            <TextField id="name" onChange={this.handleChange} style={{ width: "80%" }} label="Course name" />
           </p>
           <p>
-            <TextField id="room" style={{ width: "80%" }} label="Room" />
+            <TextField id="section" style={{ width: "80%" }} label="Room" />
           </p>
           <p>
             <TextField id="title" style={{ width: "80%" }} label="Title" />
