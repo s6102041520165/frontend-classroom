91316764973aaf8b0face4c14ba15a0cf7a47384
s6102041520165@email.kmutnb.ac.th <s6102041520165@email.kmutnb.ac.th>
2019-12-19 09:13:18 +0700

    google auth

--- src/views/CreateCourse.js
+++ src/views/CreateCourse.js
@@ -1,74 +1,110 @@
-import React, { Component } from 'react'
-import Button from '@material-ui/core/Button';
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import axios from "axios";
-import request from "request";
-
+import Axios from "axios";
+import { GoogleLogin } from "react-google-login";
 
 class CreateCourse extends Component {
   state = {
-    name: '',
-    section: '',
-    title: '',
-  }
+    name: "",
+    section: "",
+    title: ""
+  };
 
-  handleChange = event => {
+  responseGoogle = response => {
+    console.log(response);
+  };
+
+  //Form name
+  handleChangeName = event => {
     this.setState({
-      name: event.target.value,
-      section: event.target.value,
-      title: event.target.value,
+      name: event.target.value
     });
+  };
 
-    //console.log(this.state.name)
-  }
+  //Form section
+  handleChangeSection = event => {
+    this.setState({
+      section: event.target.value
+    });
+  };
+
+  //Form title
+  handleChangeTitle = event => {
+    this.setState({
+      titile: event.target.value
+    });
+    console.log(event.target);
+  };
 
   handleSubmit = event => {
     event.preventDefault();
-    console.log(event)
+    console.log(event);
 
     const classroom = {
       name: this.state.name,
       section: this.state.section,
-      title: this.state.title,
-    }
+      title: this.state.title
+    };
     //console.log(classroom)
-    request.post(
-      {
-        url: "https://lineappbackend.herokuapp.com/webhook/course/create-course/",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(classroom)
-      },
-      (err, res, body) => {
-        console.log("status = " + JSON.stringify(body));
-      }
-    );
-
-  }
+    Axios.post(
+      "https://lineappbackend.herokuapp.com/webhook/course/create/",
+      classroom
+    ).then(res => {
+      console.log(res.data);
+    });
+  };
   render() {
     return (
       <div>
         <h1>Create Course</h1>
         <form autoComplete="off" onSubmit={this.handleSubmit}>
           <p>
-            <TextField id="name" onChange={this.handleChange} style={{ width: "80%" }} label="Course name" />
+            <TextField
+              id="name"
+              value={this.state.name}
+              onChange={this.handleChangeName.bind(this)}
+              style={{ width: "80%" }}
+              label="Course name"
+            />
           </p>
           <p>
-            <TextField id="section" onChange={this.handleChange} style={{ width: "80%" }} label="Room" />
+            <TextField
+              id="section"
+              value={this.state.section}
+              onChange={this.handleChangeSection.bind(this)}
+              style={{ width: "80%" }}
+              label="Room"
+            />
           </p>
           <p>
-            <TextField id="title" onChange={this.handleChange} style={{ width: "80%" }} label="Title" />
+            <TextField
+              id="title"
+              value={this.state.title}
+              onChange={this.handleChangeTitle.bind(this)}
+              style={{ width: "80%" }}
+              label="Title"
+            />
           </p>
           <p>
             <Button type="submit" variant="contained" color="primary">
               Create course.
             </Button>
+
+            <GoogleLogin
+              clientId="308789454611-isrob12j0f3l23meqnl8959lvdfgdg67.apps.googleusercontent.com"
+              buttonText="Login"
+              onSuccess={this.responseGoogle}
+              onFailure={this.responseGoogle}
+              cookiePolicy={"single_host_origin"}
+              scope="https://www.googleapis.com/auth/classroom.courses https://www.googleapis.com/auth/classroom.courses.readonly"
+              redirectUri="http://localhost:3000/create-course"
+            />
           </p>
         </form>
-
       </div>
-    )
+    );
   }
 }
 
-
-export default CreateCourse
+export default CreateCourse;
