687bd3bc7f6591abfd47754a26a0d60977909f26
s6102041520165@email.kmutnb.ac.th <s6102041520165@email.kmutnb.ac.th>
2020-01-01 14:58:55 +0700

    redux map to reactjs

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
 //Import module
-import { Route } from 'react-router-dom'
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import HomeIcon from '@material-ui/icons/Home';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import PersonPinIcon from '@material-ui/icons/PersonPin';
+import { Route } from "react-router-dom";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import HomeIcon from "@material-ui/icons/Home";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import PersonPinIcon from "@material-ui/icons/PersonPin";
 import { Link } from "react-router-dom";
-
-
+import { connect } from "react-redux";
 
 //Import screen
 import CreateCourse from "./views/CreateCourse";
 import UpdateCourse from "./views/UpdateCourse";
 import Home from "./views/Home";
+import { increment } from "./reducers/actions";
+import { decrement } from "./reducers/actions";
 
 const useStyles = makeStyles({
   root: {
     flexGrow: 1,
-    maxWidth: '100%',
+    maxWidth: "100%"
   },
   container: {
     maxWidth: 800,
-    margin: 'auto',
+    margin: "auto"
   }
 });
 
-export default function App() {
+const App = ({ message, counter, dispatch }) => {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
@@ -48,18 +49,45 @@ export default function App() {
           textColor="primary"
           aria-label="icon tabs example"
         >
-          <Tab tab={<Link to="/"/>} icon={<HomeIcon />} aria-label="Home" />
-          <Tab tab={<Link to="/create-course"/>} icon={<FavoriteIcon />} aria-label="favorite" />
+          <Tab tab={<Link to="/" />} icon={<HomeIcon />} aria-label="Home" />
+          <Tab
+            tab={<Link to="/create-course" />}
+            icon={<FavoriteIcon />}
+            aria-label="favorite"
+          />
           <Tab icon={<PersonPinIcon />} aria-label="person" />
         </Tabs>
-
       </Paper>
 
-      <div style={{ margin: 'auto', maxWidth: "800px", textAlign: 'center' }}>
+      <div style={{ margin: "auto", maxWidth: "800px", textAlign: "center" }}>
+        <h1>Counter : {counter}</h1>
+        <button
+          onClick={() => dispatch(increment(1))}
+          className="button is-info"
+        >
+          Increment
+        </button>
+
+        <button
+          onClick={() => dispatch(decrement(1))}
+          className="button is-info"
+        >
+          Decrement
+        </button>
         <Route path="/create-course" component={CreateCourse} />
         <Route path="/update-course" component={UpdateCourse} />
         <Route path="/" component={Home} />
       </div>
     </div>
   );
-}
+};
+
+const mapStateToProps = function(state) {
+  return {
+    message: "This is message from mapStateToProps",
+    counter: state.counters || 0
+  };
+};
+
+const AppWithConnect = connect(mapStateToProps)(App);
+export default AppWithConnect;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+const store = createStore(rootReducer)
 
 const AppWithRouter = () => (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
 
-ReactDOM.render(<AppWithRouter />, document.getElementById('root'));
+ReactDOM.render(<AppWithRouter />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
