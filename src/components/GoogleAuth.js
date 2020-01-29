e4510c01fa05f3f46c13855e74e026a89129e139
s6102041520165@email.kmutnb.ac.th <s6102041520165@email.kmutnb.ac.th>
2020-01-29 22:40:26 +0700

    added

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -42,7 +42,7 @@ const App = ({
     onFailure={onFailLogin}
     cookiePolicy={"single_host_origin"}
     scope="https://www.googleapis.com/auth/classroom.courses https://www.googleapis.com/auth/classroom.courses.readonly https://www.googleapis.com/auth/classroom.rosters https://www.googleapis.com/auth/classroom.coursework.students.readonly https://www.googleapis.com/auth/classroom.coursework.me.readonly https://www.googleapis.com/auth/classroom.coursework.students https://www.googleapis.com/auth/classroom.coursework.me"
-    redirectUri="https://lineclassroom.herokuapp.com/"
+    redirectUri="https://lineclassroom.herokuapp.com/redirect"
     style={{ position: "absolute", marginTop: "50%" }}
   />
 );
