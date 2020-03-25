70eade25f662e43c5357a47b2c742f9966b43d60
Weerachai <s6102041520165@email.com>
2020-03-25 16:07:50 +0700

    remove

--- src/map-state/google-map-state.js
+++
@@ -1,8 +0,0 @@
-export default function(state) {
-  return {
-    message: "This is message from mapStateToProps",
-    Tokens: state.token || null,
-    GoogleId: state.googleId || null,
-    Permissions: state.permissions || null
-  };
-};
