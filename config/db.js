515334dee4461947660379d97be43212532deaac
Weerachai <s6102041520165@email.kmutnb.ac.th>
2020-05-31 11:54:15 +0700

    updated mongodb configuration

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_CONNECT, { 
   useNewUrlParser: true,
-  useUnifiedTopology: true 
+  useUnifiedTopology: true ,
+  useFindAndModify: false,
 },() => {
   console.log('Connected to mongodb')
 });
